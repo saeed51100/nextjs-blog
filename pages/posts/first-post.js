85f5dedd3471c90ff81304b5faf14e47d30e01cb
saeed javadzadeh <saeedblack77@gmail.com>
2023-03-23 11:13:24 +0430

    laptop 1402/01/03 11:13

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,17 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../../components/layout';
+
 export default function FirstPost() {
-    return <h1>First Post</h1>;
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">← Back to home</Link>
+            </h2>
+        </Layout>
+    );
 }
