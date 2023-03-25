d1666f799a07a0c8375209b8f9c75f1c0c0c24da
saeed <saeedblack77@gmail.com>
2023-03-25 22:23:01 +0330

    khooneh 1402/01/05 17:32

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,24 @@
+import Head from 'next/head';
 import Layout from '../../components/layout'
 import {getAllPostIds, getPostData} from '../../lib/posts'
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
-export default function Post({postData}) {
+export default function Post({ postData }) {
     return (
         <Layout>
-            {postData.title}
-            <br/>
-            {postData.id}
-            <br/>
-            {postData.date}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
-    )
+    );
 }
 
 export async function getStaticPaths() {
@@ -21,11 +29,13 @@ export async function getStaticPaths() {
     }
 }
 
-export async function getStaticProps({params}) {
-    const postData = getPostData(params.id)
+export async function getStaticProps({ params }) {
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id);
+
     return {
         props: {
-            postData
-        }
-    }
+            postData,
+        },
+    };
 }
